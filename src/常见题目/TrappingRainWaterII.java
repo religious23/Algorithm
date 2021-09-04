d19bf7e16454b2d2840fe8b3ff546ffba0efac9f
wangwen <1035354510@qq.com>
2021-09-04 19:17:32 +0800

    二维积水问题

---
+++ src/常见题目/TrappingRainWaterII.java
@@ -0,0 +1,14 @@
+package 常见题目;
+
+/**
+ * @author 王文
+ * @version : V1.0
+ * @className: TrappingRainWaterII
+ * @description: 能积水问题 二维版
+ * @date 2021-09-04
+ */
+public class TrappingRainWaterII {
+    public static int trapRainWater(int[][] heightMap){
+        return 0;
+    }
+}
