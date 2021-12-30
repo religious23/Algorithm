2d9795948d023fb99aa0280247f8ac95323ecc73
offcn-java <myqq@qq.com>
2021-12-30 22:41:42 +0800

    每日一题 21-12-18

---
+++ src/每日一题/LK20211218.java
@@ -0,0 +1,14 @@
+package 每日一题;
+
+/**
+ * @author W.Wen
+ * @version : V1.0
+ * @className: LK20211218
+ * @description: 甲板上的战舰
+ * @date 2021-12-18
+ */
+public class LK20211218 {
+    public int countBattleships(char[][] board) {
+        return 0;
+    }
+}
